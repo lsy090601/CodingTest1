a6fc19237e0a40be967e7de3a8e669efb989c0ef
서영 <s2527@e-mirim.hs.kr>
2025-06-17 11:04:32 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double num1 = scan.nextInt();
+        double num2  = scan.nextInt();
+
+        double result = num1/num2;
+
+        System.out.println(result);
+    }
+}
