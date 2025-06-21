c0aaaba05cf61776841d1fdcbbbd94df79dfab47
서영 <s2527@e-mirim.hs.kr>
2025-06-21 19:42:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 180 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import  java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int tayear = scan.nextInt();
+        int koyear = tayear - 543;
+        System.out.println(koyear);
+    }
+}
