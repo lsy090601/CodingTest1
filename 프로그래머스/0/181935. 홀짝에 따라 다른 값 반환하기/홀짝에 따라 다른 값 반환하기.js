79316d89e122bdcf35220c3e4f599d8b3906af35
서영 <s2527@e-mirim.hs.kr>
2026-06-30 08:15:42 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.08 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = 0;
+
+    if (n % 2 === 1) { // n이 홀수
+        for (let i = 1; i <= n; i += 2) {
+            answer += i;
+        }
+    } else { // n이 짝수
+        for (let i = 2; i <= n; i += 2) {
+            answer += i * i;
+        }
+    }
+
+    return answer;
+}
